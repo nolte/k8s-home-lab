979b7f9b52a0bbe3291ebc16c9397c29ada12a47
nolte <nolte07@gmail.com>
2023-02-22 11:08:46 +0100

    Feature/audio station (#529)

--- hack/bootstrap/module_argocd/main.tf
+++ hack/bootstrap/module_argocd/main.tf
@@ -10,3 +10,22 @@ resource "helm_release" "argocd" {
     var.values_file != "" ? file(join("/",[path.module,var.values_file])) : ""
   ]
 } 
+
+resource "helm_release" "argocd_apps" {
+  depends_on = [
+    helm_release.argocd
+  ]
+  name       = "argocd-apps"
+  #repository = "https://charts.bitnami.com/bitnami"
+  #name = "argocd"
+  chart     = "../../../src/applications/argo-cd/deploy/chart-apps"
+  namespace = "argocd"
+  # wait maximal 15 mins
+  timeout   = 900 
+  create_namespace = true
+  values = [
+    var.values_file != "" ? file(join("/",[path.module,var.values_file])) : ""
+  ]
+} 
+
+
